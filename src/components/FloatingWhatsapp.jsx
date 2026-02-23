e7d267b3269b223f72d1034285c48fcc11d569e4
leemageorge555 <leemageorge555@gmail.com>
2026-02-23 14:10:58 +0530

    whats app upadted

--- src/components/FloatingWhatsapp.jsx
+++ src/components/FloatingWhatsapp.jsx
@@ -5,9 +5,9 @@ import whatsapp from "../assets/icon/whatsapp.svg"
 
 const FloatingWhatsapp = () => {
   return (
-    <div className='fixed bottom-5 right-3'>
+    <div className='fixed max-w-xl bottom-5 left-3'>
         <Link href="https://wa.me/919633443047" target='_blank'>
-        <Image src={whatsapp} alt='whatsapp' width={50} />
+        <Image src={whatsapp} alt='whatsapp' width={50} height={50} />
         </Link>
     </div>
   )
